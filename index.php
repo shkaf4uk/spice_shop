bb98c96e327ca75ad38413d7060e0fb1c20582a9
shkaf4uk <shafarenkomaksym@gmail.com>
2020-11-04 20:35:44 +0200

    slide

--- index.php
+++ index.php
@@ -60,6 +60,36 @@
 	<!--=============== End of Category Product ==================-->
 	
 	<!--=================== Product Slider =======================-->
+
+	<div class="container-prod">
+  <div class="cursive-philosopher label-prod-slider">Наши товары:</div>
+  <div class="slider-product owl-carousel">
+   <div class="prod-item product">
+    <img class="images" src="assets/images/spice-prod_6.png" alt="slider">
+    <a class="title-prod">Percent Ashitaba, Чай Percent Ashitaba, 40 чайных пакетиков<br>1,41 унц.</a>
+   </div><!--item-->
+   <div class="prod-item product">
+    <img class="images" src="assets/images/spice-prod_5.png" alt="slider">
+    <a class="title-prod">Кофе Illy Espresso молотый<br>250 г</a>
+   </div><!--item-->
+   <div class="prod-item product">
+    <img class="images" src="assets/images/spice-prod_4.png" alt="slider">
+    <a class="title-prod">Кофе CafeBoutique Colombia Supremo+India Malabar AA в зернах<br>1000 г</a>
+   </div><!--item-->
+   <div class="prod-item product">
+    <img class="images" src="assets/images/spice-prod_3.png" alt="slider">
+    <a class="title-prod">Simply Organic, Лимонный перец<br>90 г (3,17 унции)</a>
+   </div><!--item-->
+   <div class="prod-item product">
+    <img class="images" src="assets/images/spice-prod_2.png" alt="slider">
+    <a class="title-prod">Simply Organic, Органическая копченая паприка<br>2,72 унций (77 г)</a>
+   </div><!--item-->
+   <div class="prod-item product">
+    <img class="images" src="assets/images/spice-prod_1.png" alt="slider">
+    <a class="title-prod">Шафран Министерство Специй<br>1г</a>
+   </div><!--item-->
+  </div>
+ </div>
 	
 	<!--=============== End of Product Slider ===================-->
 
