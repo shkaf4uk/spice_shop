78a3af8b5425cecffcabe019144347a629b53533
KanAimoto <Max12395sniper@gmail.com>
2020-11-04 20:25:14 +0200

    main-page v.0.2.1

---
+++ parts/footer.php
@@ -0,0 +1,53 @@
+	<footer class="bg-yellow">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<div class="widget-page">
+						<h4 class="widget-title">Покупатель</h4>
+						<a href="404.html">Регистрация</a>
+						<a href="404.html">Мой аккаунт</a>
+					</div><!--widget-page-->
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<div class="widget-page">
+						<h4 class="widget-title">FAQ</h4>
+						<a href="404.html">Информация о заказе</a>
+						<a href="404.html">Наши гарантии</a>
+					</div><!--widget-page-->
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<div class="widget-page">
+						<h4 class="widget-title">Наша компания</h4>
+						<a href="404.html">About</a>
+						<a href="single-product.html">Наши товары</a>
+					</div><!--widget-page-->
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<div class="widget-contact">
+						<h4 class="widget-title">Найдите нас</h4>
+						<address>Улица Хрещатик 1<br>Phone: +38(068)-123-45-67<br>Email: spice@gmail.com</address>
+					</div><!--widget-contact-->
+				</div>
+			</div>
+		</div>
+		<div id="back-to-top"><i class="fa fa-angle-up"></i></div>
+	</footer>
+</div><!--wrapper-->
+<script src="assets/js/lib/jquery.js"></script>
+<script src="assets/js/lib/bootstrap.min.js"></script>
+<script src="assets/js/lib/owl.carousel.min.js"></script>
+<script src="assets/js/lib/jquery-ui.min.js"></script>
+<script src="assets/js/lib/TweenMax.min.js"></script>
+<script src="assets/js/lib/lazyYT.js"></script>
+<script src="assets/js/lib/masonry.pkgd.min.js"></script>
+<script src="assets/js/lib/jquery.filterizr.min.js"></script>
+<script src="assets/js/lib/slick.min.js"></script>
+<script src="assets/js/lib/jquery.counterup.min.js"></script>
+<script src="assets/js/lib/waypoints.min.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
