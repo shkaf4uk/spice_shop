c70e5a720339cceb6b663265a75c703c8e1cf508
Posse1don <7985858@gmail.com>
2020-11-04 19:06:54 +0200

    Админка

--- HTML/configs/db.php
+++ HTML/configs/db.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "spice_shop";
+$dbname = "tw";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
---
+++ admin/users.php
@@ -0,0 +1,14 @@
+<?php
+
+    $page = "users";
+
+    include $_SERVER['DOCUMENT_ROOT'] . '/HTML/configs/db.php';
+
+    include $_SERVER['DOCUMENT_ROOT'] . '/admin/parts/header.php';
+    
+?>
+<?php
+
+    include $_SERVER['DOCUMENT_ROOT'] . '/admin/parts/footer.php';
+
+?>
