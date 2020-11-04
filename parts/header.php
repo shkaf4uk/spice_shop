9dc2ff9d7c6c74b5238944a6f4543329f4f48f1a
shkaf4uk <shafarenkomaksym@gmail.com>
2020-11-04 19:38:24 +0200

    header

--- parts/header.php
+++ parts/header.php
@@ -31,137 +31,136 @@
 <body>
 <div class="wrapper">
 
-<header>
-	<div class="top-bar bg-black">
-		<div class="container-large">
+	<header>
+		<div class="top-bar bg-black">
+			<div class="container-large">
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-xs-12 text-left">
+						<ul>
+							<li><a class="cursive-philosopher con-us-header" href="contact.html">Написать нам</a></li>
+							<li><a class="cursive-philosopher" href="contact.html">Поддержка</a></li>
+						</ul><!--right-top-bar-->
+					</div>
+					<div class="col-md-6 col-sm-6 col-xs-12 text-right">
+						<ul>
+							<li><a class="cursive-philosopher reg-header" href="#">Регистрация</a></li>
+							<li><a class="cursive-philosopher" href="#">Авторизация</a></li>
+						</ul><!--right-top-bar-->
+					</div>
+				</div>
+			</div>
+		</div><!--top-bar-->
+		<div class="container-large header">
 			<div class="row">
-				<div class="col-md-6 col-sm-6 col-xs-12 text-left">
-					<ul>
-						<li><a class="cursive-philosopher con-us-header" href="contact.html">Написать нам</a></li>
-						<li><a class="cursive-philosopher" href="contact.html">Поддержка</a></li>
-					</ul><!--right-top-bar-->
+				<div class="col-md-5 col-sm-4 col-xs-4">
+					<ul class="menu">
+						<li><a class="cursive-philosopher" href="/">Главная</a></li>
+						<li class="mega-menu-top">
+							<a href="#" class="cursive-philosopher">Товары</a>
+							<div class="cursive-philosopher menu-w-stuff mega-menu columns-4">
+								<ul>
+									<li class="first">Специи</li>
+									<li><a href="#">Бадьян</a></li>
+									<li><a href="#">Базилик</a></li>
+									<li><a href="#">Молотый имбирь</a></li>
+									<li><a href="#">Какао</a></li>
+									<li><a href="#">Кориандр</a></li>
+									<li><a href="#">Корица</a></li>
+									<li><a href="#">Кунжут</a></li>
+									<li><a href="#">Куркума</a></li>
+								</ul>
+								<ul>
+									<li class="first">Чай</li>
+									<li><a href="#">Травянной</a></li>
+									<li><a href="#">Заварки</a></li>
+									<li><a href="#">Пакетики</a></li>
+									<li><a href="#">Подарочные наборы</a></li>
+									<li><a href="#">Белый чай</a></li>
+									<li><a href="#">Улуни</a></li>
+									<li><a href="#">Шу Пуер</a></li>
+									<li><a href="#">Шен Пуер</a></li>
+									<li><a href="#">Матча</a></li>
+								</ul>
+								<ul>
+									<li class="first">Сухофрукты и <br>орехи</li>
+									<li><a href="#">Финики</a></li>
+									<li><a href="#">Черника</a></li>
+									<li><a href="#">Изюм</a></li>
+									<li><a href="#">Курага</a></li>
+									<li><a href="#">Кешью</a></li>
+									<li><a href="#">Миндаль</a></li>
+									<li><a href="#">Фисташки</a></li>
+								</ul>
+								<ul>
+									<li class="first">Кофе</li>
+									<li><a href="#">Кофе в зёрнах</a></li>
+									<li><a href="#">Кофе для фильтров</a></li>
+									<li><a href="#">Кофе для еспресо</a></li>
+									<li><a href="#">Кофе для гейзера</a></li>
+									<li><a href="#">Кофе для джезви</a></li>
+									<li><a href="#">Подарочные наборы</a></li>
+									<li><a href="#">Кофе в дрип-пакетах</a></li>
+								</ul>
+							</div><!--mega-menu-->
+						</li><!--children-->
+						<li class="children">
+							<a class="cursive-philosopher" href="about.html">О нас</a>
+							<ul class="sub-menu">
+								<li><a href="#">Почему именно SpecialSpicy?</a></li>
+								<li><a href="#">Сертификаты качества</a></li>
+								<li><a href="#">Договора продажи</a></li>
+							</ul><!--sub-menu-->
+						</li>
+						<li><a class="cursive-philosopher" href="blog.html">Рецепты</a></li>
+						<li><a class="cursive-philosopher" href="journal-blog.html">Новости</a></li>
+						<li class="mega-menu-top">
+							<a class="cursive-philosopher" href="#">Партнеры</a>
+							<div class="menu-w-stuff mega-menu columns-3">
+								<ul>
+									<li class="first">Shop Page</li>
+									<li><a href="shop.html">Shop</a></li>
+									<li><a href="shop-two.html">Shop v2</a></li>
+									<li><a href="shop-column-three.html">Shop Column 3</a></li>
+									<li><a href="shop-column-three-v2.html">Shop Column 3 v2</a></li>
+									<li><a href="shop-column-three-v3.html">Shop Column 3 v3</a></li>
+								</ul>
+								<ul>
+									<li class="first">Shop Page</li>
+									<li><a href="shop-with-sidebar.html">Shop With Sidebar</a></li>
+									<li><a href="shop-with-sidebar-v2.html">Shop With Sidebar v2</a></li>
+									<li><a href="shop-column-six.html">Shop Column Six</a></li>
+									<li><a href="shop-full-width.html">Shop Full Width</a></li>
+									<li><a href="shop-categories.html">Shop Categories</a></li>
+								</ul>
+								<ul>
+									<li class="first">Single Product</li>
+									<li><a href="single-product-limited.html">Single Product Limited</a></li>
+									<li><a href="single-product-sale.html">Single Product Sale</a></li>
+									<li><a href="single-product-stock.html">Single Product Stock</a></li>
+									<li><a href="single-product-v2.html">Single Product v2</a></li>
+									<li><a href="single-product-video.html">Single Product Video</a></li>
+								</ul>
+							</div><!--mega-menu-->
+						</li>
+					</ul><!--menu-->
+					<button type="button" class="menu-button">
+						<span></span>
+					</button>
 				</div>
-				<div class="col-md-6 col-sm-6 col-xs-12 text-right">
-					<ul>
-						<li><a class="cursive-philosopher reg-header" href="#">Регистрация</a></li>
-						<li><a class="cursive-philosopher" href="#">Авторизация</a></li>
-					</ul><!--right-top-bar-->
+				<div class="logotype col-md-2 col-sm-3 col-xs-3 text-center">
+					<div class="logo">
+						<a href="/">
+							<img src="assets/images/logo1.png" alt="logo">
+						</a>
+					</div>
 				</div>
-			</div>
-		</div>
-	</div><!--top-bar-->
-	<div class="container-large header">
-		<div class="row">
-			<div class="col-md-5 col-sm-4 col-xs-4">
-				<ul class="menu">
-					<li><a class="cursive-philosopher" href="/">Главная</a></li>
-					<li class="mega-menu-top">
-						<a href="#" class="cursive-philosopher">Товары</a>
-						<div class="menu-w-stuff mega-menu columns-4">
-							<ul>
-								<li class="first">Fruit &amp; Veg</li>
-								<li><a href="#">Set Boxes</a></li>
-								<li><a href="#">Fruit</a></li>
-								<li><a href="#">Veg</a></li>
-								<li><a href="#">Salad</a></li>
-								<li><a href="#">Herbs</a></li>
-								<li><a href="#">Organic</a></li>
-								<li><a href="#">Juice &amp; Smoothies</a></li>
-								<li><a href="#">Nuts &amp; Dried Fruit</a></li>
-								<li><a href="#">Gourmet Pantry</a></li>
-							</ul>
-							<ul>
-								<li class="first">Dairy</li>
-								<li><a href="#">Milk</a></li>
-								<li><a href="#">Cheese</a></li>
-								<li><a href="#">Specialty Cheese</a></li>
-								<li><a href="#">Yoghurt</a></li>
-								<li><a href="#">Butter</a></li>
-								<li><a href="#">Cream</a></li>
-								<li><a href="#">Dips &amp; Antipasto</a></li>
-							</ul>
-							<ul>
-								<li class="first">Frozen</li>
-								<li><a href="#">Frozen Vegetables</a></li>
-								<li><a href="#">Frozen Desserts &amp; Bakery</a></li>
-								<li><a href="#">Frozen Seafood</a></li>
-								<li><a href="#">Frozen Breakfast Food</a></li>
-								<li><a href="#">Frozen Fruit</a></li>
-								<li><a href="#">Ice</a></li>
-								<li><a href="#">Frozen Appetizers</a></li>
-							</ul>
-							<ul>
-								<li class="first">Superfoods</li>
-								<li><a href="#">Ayurverdic</a></li>
-								<li><a href="#">South American</a></li>
-								<li><a href="#">Green &amp; Fungi</a></li>
-								<li><a href="#">Cacao &amp; Coconut</a></li>
-								<li><a href="#">Fruit &amp; Hemp</a></li>
-								<li><a href="#">Superfood Teas</a></li>
-								<li><a href="#">Superfood Snacks</a></li>
-								<li><a href="#">Superfood Juices</a></li>
-							</ul>
-						</div><!--mega-menu-->
-					</li><!--children-->
-					<li class="children">
-						<a class="cursive-philosopher" href="about.html">О нас</a>
-						<ul class="sub-menu">
-							<li><a href="#">Почему именно SpecialSpicy?</a></li>
-							<li><a href="#">Organic Leadership Awards</a></li>
-							<li><a href="#">Verification &amp; Certification</a></li>
-							<li><a href="#">Organic Standards</a></li>
-						</ul><!--sub-menu-->
-					</li>
-					<li><a class="cursive-philosopher" href="blog.html">Рецепты</a></li>
-					<li><a class="cursive-philosopher" href="journal-blog.html">Новости</a></li>
-					<li class="mega-menu-top">
-						<a class="cursive-philosopher" href="#">Партнеры</a>
-						<div class="menu-w-stuff mega-menu columns-3">
-							<ul>
-								<li class="first">Shop Page</li>
-								<li><a href="shop.php">Shop</a></li>
-								<li><a href="shop-two.html">Shop v2</a></li>
-								<li><a href="shop-column-three.html">Shop Column 3</a></li>
-								<li><a href="shop-column-three-v2.html">Shop Column 3 v2</a></li>
-								<li><a href="shop-column-three-v3.html">Shop Column 3 v3</a></li>
-							</ul>
-							<ul>
-								<li class="first">Shop Page</li>
-								<li><a href="shop-with-sidebar.html">Shop With Sidebar</a></li>
-								<li><a href="shop-with-sidebar-v2.html">Shop With Sidebar v2</a></li>
-								<li><a href="shop-column-six.html">Shop Column Six</a></li>
-								<li><a href="shop-full-width.html">Shop Full Width</a></li>
-								<li><a href="shop-categories.html">Shop Categories</a></li>
-							</ul>
-							<ul>
-								<li class="first">Single Product</li>
-								<li><a href="single-product-limited.html">Single Product Limited</a></li>
-								<li><a href="single-product-sale.html">Single Product Sale</a></li>
-								<li><a href="single-product-stock.html">Single Product Stock</a></li>
-								<li><a href="single-product-v2.html">Single Product v2</a></li>
-								<li><a href="single-product-video.html">Single Product Video</a></li>
-							</ul>
-						</div><!--mega-menu-->
-					</li>
-				</ul><!--menu-->
-				<button type="button" class="menu-button">
-					<span></span>
-				</button>
-			</div>
-			<div class="logotype col-md-2 col-sm-3 col-xs-3 text-center">
-				<div class="logo">
-					<a href="/">
-						<img src="assets/images/logo1.png" alt="logo">
-					</a>
-				</div>
-			</div>
-			<div class="col-md-5 col-sm-5 col-xs-5 text-right">
-				<ul class="info-header">
-					<li><a href="#"><i class="fa fa-volume-control-phone"></i>+38(068)-123-45-67</a></li>
-					<li class="cursive-philosopher" class="search-icon"><a href="#"><i class="fa fa-search"></i>Искать</a></li>
-					<li class="cart">
-						<a class="cursive-philosopher" href="cart.php"><i class="fa fa-shopping-bag"></i>Корзина (0)</a>
-						<div class="cart-modal">
+				<div class="col-md-5 col-sm-5 col-xs-5 text-right">
+					<ul class="info-header">
+						<li><a href="#"><i class="fa fa-volume-control-phone"></i>+38(068)-123-45-67</a></li>
+						<li class="cursive-philosopher" class="search-icon"><a href="#"><i class="fa fa-search"></i>Искать</a></li>
+						<li class="cart">
+							<a class="cursive-philosopher" href="cart.html"><i class="fa fa-shopping-bag"></i>Корзина (0)</a>
+							<div class="cart-modal">
 
 <?php 
 		  			//Вывод добавленных товаров в модальном окне (при наведении на корзину)
